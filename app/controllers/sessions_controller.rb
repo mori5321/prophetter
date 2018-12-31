f389fafeaae507262d389d0b472cbe1269faefe8
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-12-31 20:30:55 +0900

    skip before action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate!, only: :create
+
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id #TODO: Session HiJack
