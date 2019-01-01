1a418c7b01cdb84ab3bd72d96659007b164f8819
西森大介 <nishimori.daisuke@di-v.co.jp>
2019-01-02 02:51:23 +0900

    menu bar

--- app/controllers/prophets_controller.rb
+++ app/controllers/prophets_controller.rb
@@ -2,7 +2,7 @@ class ProphetsController < ApplicationController
   skip_before_action :authenticate!, only: :index
 
   def index
-    @prophets = Prophet.not_published.publishing_order.page(params[:page]).per(3)
+    @prophets = Prophet.includes(:user).not_published.publishing_order.page(params[:page]).per(10)
   end
 
   def new
