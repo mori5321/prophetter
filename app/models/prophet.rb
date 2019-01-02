c337ccd129dc37bff0d296ddd65d059af1d1f97c
西森大介 <nishimori.daisuke@di-v.co.jp>
2019-01-03 00:19:57 +0900

    a

--- app/models/prophet.rb
+++ app/models/prophet.rb
@@ -23,6 +23,6 @@ class Prophet < ApplicationRecord
 
   private
     def tweet_text
-      "https://prophetter.herokuapp.com/prophets/#{self.id}  #prophetterで目標達成"
+      "https://prophetter.herokuapp.com/prophets/#{self.id} #prophetter"
     end
 end
